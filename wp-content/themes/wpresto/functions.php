c9a725caf721da894263279204e92f7556c682df
AhmedSoleiman <ahmed.soleiman.mad@gmail.com>
2020-08-28 09:20:57 +0200

    Page menu Ok and footer

--- wp-content/themes/wpresto/functions.php
+++ wp-content/themes/wpresto/functions.php
@@ -1,22 +1,26 @@
-<?php 
+<?php
 
 /*Function to Add style*/
 
-function add_styles(){
-    wp_enqueue_style("mainStyle" , get_template_directory_uri()."/style.css");
+function add_styles()
+{
+    wp_enqueue_style("mainStyle", get_template_directory_uri() . "/style.css");
+    wp_enqueue_style("menuStyle", get_template_directory_uri() . "/menu.css");
 }
 
 
-function montheme_supports () {
+function montheme_supports()
+{
     add_theme_support('title-tag');
     add_theme_support('menus');
-    add_theme_support( 'post-thumbnails' );
+    add_theme_support('post-thumbnails');
     register_nav_menu('header', 'En tête du menu');
 }
 
-function montheme_register_assets () {
+function montheme_register_assets()
+{
     wp_register_style('bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css');
-    wp_register_script('bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js', ['popper', 'jquery'], false, true);  
+    wp_register_script('bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js', ['popper', 'jquery'], false, true);
     wp_register_script('popper', 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js', [], false, true);
     wp_deregister_script('jquery');
     wp_register_script('jquery', 'https://code.jquery.com/jquery-3.2.1.slim.min.js', [], false, true);
@@ -24,16 +28,70 @@ function montheme_register_assets () {
     wp_enqueue_script('bootstrap');
 }
 
-function montheme_title_separator () {
+function montheme_title_separator()
+{
     return '|';
 }
 
 
 // Add Actions
 
-add_action( 'wp_enqueue_scripts', 'add_styles' );
+add_action('wp_enqueue_scripts', 'add_styles');
 add_action('after_setup_theme', 'montheme_supports');
 add_action('wp_enqueue_scripts', 'montheme_register_assets');
 add_filter('document_title_separator', 'montheme_title_separator');
 
-?>
+
+
+
+
+/*Create custom post recipes*/
+
+function custom_post_recipe()
+{
+
+    $args = array(
+
+        'labesl' => array(
+
+            'name' => "Recipes",
+            'singular_name' => "Recipe"
+        ),
+
+        'hierarchical' => false,
+        'public' => true,
+        'menu_icon' => 'dashicons-food',
+        'has_archive' => true,
+        'supports' => array('title', 'editor', 'thumbnail'),
+    );
+
+    register_post_type("recipes", $args);
+}
+
+add_action('init', 'custom_post_recipe');
+
+
+function taxonomy_recipes()
+{
+
+    $args = array(
+
+        'labels' => array(
+            'name' => "Origins",
+            'singular_name' => "Origin"
+        ),
+
+        'public' => true,
+        'hierarchical' => false,
+    );
+
+    register_taxonomy('origin', array('recipes'), $args);
+}
+
+add_action('init', 'taxonomy_recipes');
+
+
+if (function_exists('acf_add_options_page')) {
+
+    acf_add_options_page();
+}
---
+++ wp-content/themes/wpresto/menu.php
@@ -0,0 +1,90 @@
+<?php
+
+/**
+ * Template Name: menu
+ *
+ * 
+ */
+?>
+
+<?php
+
+/*Variable champs*/
+
+$image = get_field("image_showcas");
+$subtitle = get_field("subtitle");
+$main_title = get_field("main_title");
+$menu = get_field("add_menu_category");
+$subtitle_menu = get_field("subtitle-menu");
+$title_menu = get_field("title-menu");
+
+?>
+
+<?php get_header(); ?>
+<div class="show_case_menu" style="background-image: url(<?php echo $image["url"] ?>)">
+
+    <div class="heading">
+        <div class="container">
+
+            <h3><?php echo $subtitle ?></h3>
+            <h1><?php echo $main_title ?></h1>
+        </div>
+
+    </div>
+
+</div>
+<section class="menu">
+
+    <div class="menu_container">
+
+        <div class="container">
+            <div class="menu_wrap"> <?php if (have_rows('add_menu_category')) { ?>
+
+                    <div class="sm_heading">
+                        <h3><?php echo $subtitle_menu;  ?></h3>
+                        <h2><?php echo $title_menu;  ?></h2>
+
+                    </div>
+
+
+
+                    <!--<pre> <?php echo print_r($menu) ?></pre>-->
+
+                    <?php while (have_rows('add_menu_category')) : the_row(); ?>
+
+                        <h2 class="title_cat"> <?php the_sub_field('title') ?></h2>
+                        <?php if (have_rows('add_item')) {
+
+                                                while (have_rows('add_item')) : the_row(); ?>
+                                <ul class="menu_item">
+                                    <li class="price_title_menu restaurant_menu__row">
+
+                                        <div class="restaurant_menu__meal">
+                                            <span class="title_menu_item"><?php the_sub_field('title_item') ?> </span>
+                                        </div>
+                                        <span class="restaurant_menu__price"> <?php the_sub_field('price') ?></span>
+                                    </li>
+
+                                    <li class="menu_text"><?php the_sub_field('descitem') ?></li>
+                                </ul>
+
+
+
+                <?php endwhile;
+                                            }
+
+                                        endwhile;
+                                    }  ?>
+            </div>
+        </div>
+
+    </div>
+
+</section>
+
+
+
+</div>
+
+
+<?php get_footer(); ?>
